a2186461c6f11f124f9c5d6c29e5fa3ad431326f
magacula <makoigacula@gmail.com>
2021-10-06 14:31:12 -0700

    clearned React boilerpate

---
+++ src/components/Commit.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Commit = () => {
+  return <div></div>;
+};
---
+++ src/components/CommitListPage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const CommitListPage = () => {
+  return <div>Commit Page</div>;
+};
